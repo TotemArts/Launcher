66bdcbc83f1a242f3ce8451d2125f6f7d6aaf494
SonnyX <randy@vonderweide.nl>
2019-08-14 13:28:38 +0200

    Make bash scripts better

--- create-symlink-for-theme.sh
+++ create-symlink-for-theme.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo Name theme to create symlink for:
+echo "Name theme to create symlink for:"
 read theme
-if [ $( ls -1tqd ./$theme | wc -l ) == "1" ]
+if [ $( ls -1tqd "$(pwd)/$theme" | wc -l ) == "1" ]
 then
-  ln -s "./$theme" "./target/release/$theme"
-  ln -s "./$theme" "./target/debug/$theme"
+  ln -s "$(pwd)/$theme" "$(pwd)/target/release/$theme" 2>/dev/null
+  ln -s "$(pwd)/$theme" "$(pwd)/target/debug/$theme" 2>/dev/null
 else
-  echo There is no theme named \"$theme\" in your current folder
+  echo "There is no theme named \"$theme\" in your current folder"
 fi
