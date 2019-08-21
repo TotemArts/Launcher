b138d3170babcf5ef74b6936adc07ddaa5424584
SonnyX <randy@vonderweide.nl>
2019-08-21 10:57:14 +0200

    Add more scripts, improved building on linux for windows

--- package-windows.sh
+++ package-windows.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cargo build --target=x86_64-pc-windows-gnu --release
+wine ResourceHacker.exe -save ./RenegadeX-Launcher.exe -open ./target/x86_64-pc-windows-gnu/release/RenegadeX-Launcher.exe -resource RenegadeX-Launcher.res -action add
 rm RenX-Launcher.zip
-zip -j9 RenX-Launcher target/x86_64-pc-windows-gnu/release/RenegadeX-Launcher.exe sciter.dll SelfUpdateExecutor.exe
+zip -j9 RenX-Launcher RenegadeX-Launcher.exe sciter.dll SelfUpdateExecutor.exe
 zip -9 RenX-Launcher -r dom
