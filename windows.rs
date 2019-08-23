7e06b65f55ea7876c658d134a0befecae4fa58b2
SonnyX <randy@vonderweide.nl>
2019-08-23 19:54:35 +0200

    Change compilation of the windows binary once again, fix issues some bugs in the process

--- windows.rs
+++ windows.rs
@@ -1,28 +1,6 @@
-#[cfg(windows)]
-extern crate winres;
-#[cfg(windows)]
-use std::io::Write;
+extern crate embed_resource;
 
-#[cfg(windows)]
-fn main() {
-  let target_os = match std::env::var("CARGO_CFG_TARGET_OS") {
-    Ok(value) => value,
-    Err(e) =>  panic!("No environmental variable found that matches CARGO_CFG_TARGET_OS: {}", e)
-  };
-  if target_os == "windows" {
-    let mut res = winres::WindowsResource::new();
-    res.set_manifest_file("manifest.xml");
-    res.set_icon("rx.ico");
-    match res.compile() {
-      Err(e) => {
-        write!(std::io::stderr(), "windows.rs:18:{}", e).unwrap();
-        std::process::exit(1);
-      },
-      Ok(_) => {}
-    }
-  }
-}
 
-#[cfg(unix)]
 fn main() {
+    embed_resource::compile("manifest.rc");
 }
