4b8ca8f7319a3f552c5ba80e81616cfa30b86e7e
SonnyX <randy@vonderweide.nl>
2020-06-07 18:57:18 +0200

    Include proper line endings and rename launcher, closes #30

--- package-windows.sh
+++ package-windows.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cargo update --manifest-path="windows-target/Cargo.toml"
 cargo build --manifest-path="windows-target/Cargo.toml" --target-dir="windows-target" --target=x86_64-pc-windows-gnu --release
-cp ./windows-target/x86_64-pc-windows-gnu/release/RenegadeX-Launcher.exe ./windows-target/RenegadeX-Launcher.exe
+cp ./windows-target/x86_64-pc-windows-gnu/release/RenegadeX-Launcher.exe "./windows-target/Renegade X Launcher.exe"
 rm windows-target/RenX-Launcher.zip
-zip -j9 windows-target/RenX-Launcher windows-target/RenegadeX-Launcher.exe windows-target/sciter.dll windows-target/SelfUpdateExecutor.exe
-zip -9 windows-target/RenX-Launcher -r dom
+zip -j9 windows-target/RenX-Launcher "windows-target/Renegade X Launcher.exe" "windows-target/sciter.dll" "windows-target/SelfUpdateExecutor.exe"
+zip -9 windows-target/RenX-Launcher -r "dom"
