9ee21112299347e17b3f64ab397d13f0e083b03d
SonnyX <randy@vonderweide.nl>
2020-06-25 22:53:13 +0200

    Change build process from x86_64 to be i686, meaning 32bit support

--- resolve-link-issues.sh
+++ resolve-link-issues.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-eval "cp /usr/x86_64-w64-mingw32/lib/*.o ~$USER/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-pc-windows-gnu/lib/"
+eval "cp /usr/i686-w64-mingw32/lib/*.o ~$USER/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/i686-pc-windows-gnu/lib/"
