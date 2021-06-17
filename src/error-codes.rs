7267edb4fd04f01a529fbe69064474574a272c89
SonnyX <randy@vonderweide.nl>
2021-06-17 16:39:31 +0200

    Add new error message

--- src/error-codes.rs
+++ src/error-codes.rs
@@ -1,2 +1,3 @@
 -1073741515 //missing dependencies, install UE3Redist.exe
 3 // `cannot create D3D9 device, exiting` or `F5 Exit`, source: HIHIHI
+-1073740791 // Ran out of virtual memory (VRAM), switch to 64 bits, source: Nexus
