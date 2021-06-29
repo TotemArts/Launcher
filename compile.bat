ae2c0ea7b9dd7ad42c7ef92580b0e974a18d5aa1
SonnyX <randy@vonderweide.nl>
2021-06-29 21:09:37 +0200

    Update github actions

--- compile.bat
+++ compile.bat
@@ -1,4 +1,5 @@
 cd backend
+cross update
 cross build --release --target=i686-pc-windows-gnu
 cd ..
 echo F|xcopy /Y backend\target\i686-pc-windows-gnu\release\RenegadeX-Launcher.exe RenegadeX-Launcher.exe
