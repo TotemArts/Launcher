01ab5c2c12d198b3ffb850613514c446509e0e08
SonnyX <randy@vonderweide.nl>
2021-09-20 19:17:02 +0200

    Try-Catch anywhere

--- dom/chat.js
+++ dom/chat.js
@@ -3,7 +3,7 @@ var messages = {};
 function newMessages(id, message) {
   var shouldScroll = (document.$("div.chat").scroll("bottom") > 0);
 
-  document.$("div.chat").append("<message id=\""+id+"\">"+message+"</message>");
+  document.$("div.chat").append("<message id=\"" + id + "\">" + message + "</message>");
 
   if (!shouldScroll) {
     scrollToBottom();
