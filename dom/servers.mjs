09d10e91e467dc0264602de5258652dba60d3c4c
SonnyX <randy@vonderweide.nl>
2021-12-05 14:27:10 +0100

    Add UI functionality for servers

--- dom/servers.mjs
+++ dom/servers.mjs
@@ -9,18 +9,11 @@ class ServersTable extends Element {
     super.this?.(props);
     this.props = props;
 
-    if(globalThis.servers) {
-      console.log("servers in globalThis");
-      this.list = globalThis.servers;
+    if (globalThis.server_list) {
+      console.log("servers in globalThis.server_list");
+      this.list = globalThis.server_list.get_servers();
     } else {
-      globalThis.servers_callback = function(data) {
-        console.log("servers_callback");
-        globalThis.servers = data;
-        globalThis.callback_service.publish("servers", data);
-      }
-      globalThis.servers = [];
-      this.list = globalThis.servers;
-      Window.this.xcall("get_servers", globalThis.servers_callback);
+      this.list = [];
     }
   }
 
@@ -35,16 +28,16 @@ class ServersTable extends Element {
   }
 
   render() {
-    
+
     console.log("render ServerTable");
     let list = [];
     let totalItems = this.totalItems();
-    let {currentItem, selectedItems } = this;
+    let { currentItem, selectedItems } = this;
 
 
-    for( let index = 0; index <= totalItems; ++index ) {
+    for (let index = 0; index <= totalItems; ++index) {
       let item = this.itemAt(index);
-      if(item) list.push(this.renderItem(item,item === currentItem, selectedItems?.has(item)));
+      if (item) list.push(this.renderItem(item, item === currentItem, selectedItems?.has(item)));
     }
     return this.renderList(list);
   }
@@ -63,8 +56,7 @@ class ServersTable extends Element {
     globalThis.callback_service.unsubscribe("servers", this, this.callback);
   }
 
-  renderList(items)
-  {
+  renderList(items) {
     return <table class="servers" {...this.props}>
       <thead>
         <tr>
@@ -76,68 +68,68 @@ class ServersTable extends Element {
         </tr>
       </thead>
       <tbody>
-        { items }
+        {items}
       </tbody>
     </table>;
   }
 
   renderItem(item, isCurrent, isSelected) {
     return <tr key={item.key}>
-              <th class={this.isLocked(isCurrent)}></th>
-              <th>{item["Name"]}</th>
-              <th>{item["Current Map"]}</th>
-              <th>{item["Players"]}</th>
-              <th>Ping</th>
-            </tr>;
+      <th class={this.isLocked(isCurrent)}></th>
+      <th>{item["Name"]}</th>
+      <th>{item["Current Map"]}</th>
+      <th>{item["Players"]}</th>
+      <th>Ping</th>
+    </tr>;
   }
 
   isLocked(isCurrent) {
-    if(isCurrent)
+    if (isCurrent)
       return "locked";
     return "";
   }
-  
+
   itemOfElement(element) {
     return this.itemAt(element.elementIndex);
   }
 
   onkeydown(evt) {
     console.log("onkeydown");
-    switch(evt.code) {
-      case "KeyDOWN" : 
-        if(!this.currentItem) { 
-          this.componentUpdate({ currentItem : this.itemOfElement(0) });
+    switch (evt.code) {
+      case "KeyDOWN":
+        if (!this.currentItem) {
+          this.componentUpdate({ currentItem: this.itemOfElement(0) });
         } else {
           let index = this.indexOf(this.currentItem);
-          if( ++index < this.totalItems() ) {
-            this.componentUpdate({ currentItem : this.itemAt(index) });
+          if (++index < this.totalItems()) {
+            this.componentUpdate({ currentItem: this.itemAt(index) });
             //this.vlist.navigate("advance",index);
           }
         }
         break;
-      case "KeyUP" : 
-        if(!this.currentItem) { 
-          this.componentUpdate({ currentItem : this.itemAt(this.list.length - 1) });
+      case "KeyUP":
+        if (!this.currentItem) {
+          this.componentUpdate({ currentItem: this.itemAt(this.list.length - 1) });
         } else {
           let index = this.indexOf(this.currentItem);
-          if( --index >= 0 ) {
-            this.componentUpdate({ currentItem : this.itemAt(index) });
+          if (--index >= 0) {
+            this.componentUpdate({ currentItem: this.itemAt(index) });
             //this.vlist.navigate("advance",index);
           }
         }
         break;
-      case "KeyEND" : 
+      case "KeyEND":
         this.currentItem = this.itemAt(this.totalItems() - 1);
         //this.vlist.navigate("end");
         break;
-      case "KeyHOME" : 
+      case "KeyHOME":
         this.currentItem = this.itemAt(0);
         //this.vlist.navigate("start");
         break;
       default:
         return false;
     }
-    this.post(new Event("input", {bubbles:true}));
+    this.post(new Event("input", { bubbles: true }));
     return true;
   }
 
@@ -145,30 +137,30 @@ class ServersTable extends Element {
     console.log("setCurrentOption");
     console.log(child);
     let option;
-    for(let node = child; node; node = node.parentElement) {
-      if(node.parentElement === this) {
+    for (let node = child; node; node = node.parentElement) {
+      if (node.parentElement === this) {
         option = node;
-        break; 
+        break;
       }
     }
-    if(option) {
+    if (option) {
       console.log(this.itemOfElement(option));
-      this.componentUpdate({ currentItem : this.itemOfElement(option) });
-      this.post(new Event("input", {bubbles:true}));
+      this.componentUpdate({ currentItem: this.itemOfElement(option) });
+      this.post(new Event("input", { bubbles: true }));
       return true;
     }
   }
 
   ["on mousedown at tr"](evt) {
     console.log("mousedown");
-    if(evt.button == 1) {
+    if (evt.button == 1) {
       console.log("mousedown click");
       this.setCurrentOption(evt.target);
     }
   }
 
   get value() {
-    if(!this.currentItem) return undefined;
+    if (!this.currentItem) return undefined;
     return this.currentItem;
   }
 }
@@ -176,120 +168,180 @@ class ServersTable extends Element {
 class ServerList {
   minimum_players = 0;
   maximum_players = 64;
+  current_players = 0;
   sortBy = "Players";
   game_version = "5.48.145";
 
-  servers;
+  servers = [];
 
+  constructor() {
+    Window.this.xcall("get_servers", this.servers_callback);
+  }
 
+  refresh_servers() {
+    Window.this.xcall("get_servers", globalThis.server_list.servers_callback);
+  }
+
+  servers_callback(data) {
+    globalThis.server_list.servers = data;
+    globalThis.server_list.notify_subscribers();
+  }
 
   set_minimum_players(players) {
-    this.minimum_players = players;
+    globalThis.server_list.minimum_players = players;
     notify_subscribers();
   }
 
   set_maximum_players(players) {
-    this.maximum_players = players;
+    globalThis.server_list.maximum_players = players;
     notify_subscribers();
   }
 
   notify_subscribers() {
+    globalThis.callback_service.publish("servers", globalThis.server_list.get_servers());
+  }
+
+  get_servers() {
     const list = [];
 
-  /* Example entry of this.servers
-    {
-      "Name": "blabla",
-      "Current Map": "CNC-LakeSide",
-      "Bots": 1,
-      "Players": 0,
-      "Game Version": "5.48.145",
-      "Variables": {
-        "bPassworded": false,
-        "bAllowPrivateMessaging": true,
-        "bRanked": true,
-        "Game Type": 1,
-        "Player Limit": 10,
-        "Vehicle Limit": 11,
-        "bAutoBalanceTeams": false,
-        "Team Mode": 6,
-        "bSpawnCrates": true,
-        "CrateRespawnAfterPickup": 35.0,
-        "Time Limit": 0
+    /* Example entry of this.servers
+      {
+        "Name": "blabla",
+        "Current Map": "CNC-LakeSide",
+        "Bots": 1,
+        "Players": 0,
+        "Game Version": "5.48.145",
+        "Variables": {
+          "bPassworded": false,
+          "bAllowPrivateMessaging": true,
+          "bRanked": true,
+          "Game Type": 1,
+          "Player Limit": 10,
+          "Vehicle Limit": 11,
+          "bAutoBalanceTeams": false,
+          "Team Mode": 6,
+          "bSpawnCrates": true,
+          "CrateRespawnAfterPickup": 35.0,
+          "Time Limit": 0
+        },
+        "Port": 7777,
+        "IP": "00.00.00.143"
       },
-      "Port": 7777,
-      "IP": "00.00.00.143"
-    },
-  */
-    for(const server of this.servers) {
-      if(server["Players"] >= this.minimum_players && 
-        server["Players"] <= this.maximum_players &&
-        (!this.same_version || server["Game Version"] == this.game_version)) {
-          list.push(server);
+    */
+    globalThis.server_list.current_players = 0;
+    for (const server of globalThis.server_list.servers) {
+      globalThis.server_list.current_players += server["Players"];
+      if (server["Players"] >= globalThis.server_list.minimum_players &&
+        server["Players"] <= globalThis.server_list.maximum_players &&
+        (!globalThis.server_list.same_version || server["Game Version"] == globalThis.server_list.game_version)) {
+        list.push(server);
       }
     }
-    globalThis.callback_service.publish("servers", list);
+    console.log("total players: " + globalThis.server_list.current_players);
+    return list;
   }
 }
 
-export class Servers extends Element 
-{
-  render(props) 
-  {
+export class Servers extends Element {
+  server_list = new ServerList();
+
+  this() {
+    if(!globalThis.server_list)
+      globalThis.server_list = new ServerList();
+    this.server_list = globalThis.server_list;
+  }
+
+  componentDidMount() {
+    globalThis.callback_service.subscribe("servers", this, this.callback);
+  }
+
+  callback(server_list) {
+    console.log("Servers callback called Sjdkfja;");
+    var server_list_clone = Object.assign({}, globalThis.server_list);
+    this.componentUpdate({ server_list: server_list_clone });
+  }
+
+  componentWillUnmount() {
+    globalThis.callback_service.unsubscribe("servers", this, this.callback);
+  }
+
+  render(props) {
+    console.log("current players in this: " + this.server_list.current_players);
+    console.log("current players in globalThis: " + globalThis.server_list.current_players);
     return <div {...this.props} id="not_chat" class="join_server">
-    <div class="titlebar">
-      <h3 class="title">Servers</h3>
-      <p class="nowrap padding" style="font-size: 7pt;">There are currently <output players_online/> players online</p>
-      <div class="spacer"></div>
-      filter
-      <div class="filter down"></div>
-      <div class="refresh"></div>
-    </div>
-    <div class="filterbar">
-      <p class="nowrap">Players: <output server_filter_players_min /> - <output server_filter_players_max /></p>
-      <div class="slider" minValue="0" maxValue="64" min="0" max="64">
-        <div class="range" style="left: 0%; width: *; margin-right: 6dip;"></div>
-        <div class="handle start" style="left: -3dip;"></div>
-        <div class="handle end" style="left: auto; right: 0dip; margin-right: 0dip;"></div>
+      <div class="titlebar">
+        <h3 class="title">Servers</h3>
+        <p class="nowrap padding" style="font-size: 7pt;">There are currently { this.server_list.current_players } players online</p>
+        <div class="spacer"></div>
+        filter
+        <div class="filter down"></div>
+        <div class="refresh"></div>
       </div>
-      <checkmark class="big checked" toggle/><p class="nowrap">Same version</p>
-    </div>
-    <div class="body mheight">
-      <ServersTable />
-    </div>
-    <div class="titlebar">
-      <h3 class="title"><output title_menu/></h3>
-      <div class="spacer"></div>
-      <div class="dropdown_menu closed" onclick="this.classList.toggle('open');this.classList.toggle('closed'); document.$('div.body > div.menu').style['visibility'] = this.classList.contains('closed')?'collapse':'visible';">PLAY ONLINE</div>
-    </div>
-    <div class="body hflow">
-      <div class="menu child-padding" style="visibility: hidden;">
-        <div class="padding" overlay="ip.htm"><h4>JOIN IP</h4></div>
-        <div class="padding"><h4>LAUNCH SERVER</h4></div>
-        <div class="padding"><h4>PLAY ONLINE</h4></div>
-        <div class="padding"><h4>PLAY SKIRMISH</h4></div>
-      </div>
-      <div class="expand" style="margin-right: 10px; ">
-        <h3>MAP: <span id="map-name" style="color: #CE5135;"></span></h3>
-        <div class="hflow" style=" height: *; vertical-align: bottom;">
-          <div class="vflow expand child-padding">
-            <p>Time Limit: <span id="time-limit"></span></p>
-            <p>Vehicle Limit: <span id="vehicle-limit"></span></p>
-            <p>Player Limit: <span id="player-limit"></span></p>
-            <p>Mine Limit: <span id="mine-limit"></span></p>
-            <p>Game Mode: <span id="game-mode"></span></p>
-          </div>
-          <div class="vflow expand child-padding">
-            <p><checkmark class="checked" id="crates"/>Crates</p>
-            <p><checkmark class="checked" id="steam"/>Steam Required</p>
-            <p><checkmark class="checked" id="ranked"/>Ranked</p>
-            <p><checkmark class="checked" id="balance"/>Auto Balance</p>
-            <p><checkmark class="checked" id="infantry"/>Infantry Only</p>
-          </div>
+      <div class="filterbar">
+        <p class="nowrap">Players: { this.server_list.minimum_players } - { this.server_list.maximum_players }</p>
+        <div class="slider" minValue="0" maxValue="64" min={ this.server_list.minimum_players } max={ this.server_list.maximum_players }>
+          <div class="range" style="left: 0%; width: *; margin-right: 6dip;"></div>
+          <div class="handle start" style="left: -3dip;"></div>
+          <div class="handle end" style="left: auto; right: 0dip; margin-right: 0dip;"></div>
         </div>
-        <button class="green" style="bottom: 0px;" onclick="joinServer();">JOIN SERVER</button>
+        <checkmark class="big checked" toggle /><p class="nowrap">Same version</p>
       </div>
-      <video id="map_video" src="../../PreviewVids/Default.avi" loop/>
+      <div class="body mheight">
+        <ServersTable />
+      </div>
+      <div class="titlebar">
+        <h3 class="title"><output title_menu /></h3>
+        <div class="spacer"></div>
+        <div class="dropdown_menu closed">PLAY ONLINE</div>
+      </div>
+      <div class="body hflow">
+        <div class="menu child-padding" style="visibility: hidden;">
+          <div class="padding" overlay="ip.htm"><h4>JOIN IP</h4></div>
+          <div class="padding"><h4>LAUNCH SERVER</h4></div>
+          <div class="padding"><h4>PLAY ONLINE</h4></div>
+          <div class="padding"><h4>PLAY SKIRMISH</h4></div>
+        </div>
+        <div class="expand" style="margin-right: 10px; ">
+          <h3>MAP: <span id="map-name" style="color: #CE5135;"></span></h3>
+          <div class="hflow" style=" height: *; vertical-align: bottom;">
+            <div class="vflow expand child-padding">
+              <p>Time Limit: <span id="time-limit"></span></p>
+              <p>Vehicle Limit: <span id="vehicle-limit"></span></p>
+              <p>Player Limit: <span id="player-limit"></span></p>
+              <p>Mine Limit: <span id="mine-limit"></span></p>
+              <p>Game Mode: <span id="game-mode"></span></p>
+            </div>
+            <div class="vflow expand child-padding">
+              <p><checkmark class="checked" id="crates" />Crates</p>
+              <p><checkmark class="checked" id="steam" />Steam Required</p>
+              <p><checkmark class="checked" id="ranked" />Ranked</p>
+              <p><checkmark class="checked" id="balance" />Auto Balance</p>
+              <p><checkmark class="checked" id="infantry" />Infantry Only</p>
+            </div>
+          </div>
+          <button class="green" style="bottom: 0px;" onclick="joinServer();">JOIN SERVER</button>
+        </div>
+        <video id="map_video" src="../../PreviewVids/Default.avi" loop />
+      </div>
     </div>
-  </div>
+  }
+
+  ["on click at div.dropdown_menu"](evt, target) {
+    target.classList.toggle('open');
+    target.classList.toggle('closed');
+    document.$('div.body > div.menu').style['visibility'] = target.classList.contains('closed') ? 'collapse' : 'visible';
+  }
+
+  ["on click at div.refresh"](evt, target) {
+    globalThis.server_list.refresh_servers();
+  }
+
+  ["on click at div.filter"](evt, target) {
+    var spoiler = this.$("div.filterbar");
+    if (spoiler.style["visibility"] == "collapse") {
+      spoiler.style["visibility"] = "visible";
+    } else if (spoiler.style["visibility"] == "visible") {
+      spoiler.style["visibility"] = "collapse";
+    }
   }
 }
