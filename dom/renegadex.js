be5f0687ca07beb3a97d9774b1b46024c0c95bcd
Sean Thomas <aurorasea@gmail.com>
2022-06-12 13:37:27 +0100

    implements #46 Disable launch button when game is running Disable/ignore double click on row when game is running Renegade state notifies state of the game running

--- dom/renegadex.js
+++ dom/renegadex.js
@@ -2,15 +2,29 @@ export class RenegadeXState extends Object {
   running = false;
 
   launch_to_server(server) {
-    Window.this.xcall("launch_game", server, this.on_game_exit, this.on_game_fault);
-    running = true;
+    Window.this.xcall(
+      "launch_game",
+      server,
+      this.on_game_exit(this),
+      this.on_game_fault(this)
+    );
+    if (globalThis && globalThis.callback_service) {
+      globalThis.callback_service.publish("game_running", true);
+    }
+    this.running = true;
   }
 
-  on_game_exit() {
-    this.running = false;
+  on_game_exit(state) {
+    if (globalThis && globalThis.callback_service) {
+      globalThis.callback_service.publish("game_running", false);
+    }
+    state.running = false;
   }
 
-  on_game_fault() {
-    this.running = false;
+  on_game_fault(state) {
+    if (globalThis && globalThis.callback_service) {
+      globalThis.callback_service.publish("game_running", false);
+    }
+    state.running = false;
   }
 }
